ae501cf8fe919d79973bc9db46d9bdc2385f7019
Chidera Christian <chiderachristain36@gmail.com>
2025-06-10 22:17:42 +0100

    intialized file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,31 +26,11 @@ function Footer() {
                         </div>
                         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-5">
                             <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Location </h4>
-                                    <ul>
-                                        <li><a href="#">Advanced</a></li>
-                                        <li><a href="#"> Management</a></li>
-                                        <li><a href="#">Corporate</a></li>
-                                        <li><a href="#">Customer</a></li>
-                                        <li><a href="#">Information</a></li>
-                                    </ul>
-                                </div>
+                                
                             </div>
                         </div>
                         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Explore</h4>
-                                    <ul>
-                                        <li><a href="#">Cookies</a></li>
-                                        <li><a href="#">About</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Proparties</a></li>
-                                        <li><a href="#">Licenses</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            
                         </div>
                         <div class="col-xl-4 col-lg-4 col-md-6 col-sm-8">
                             <div class="single-footer-caption mb-50">
