5640e3aded5362645b83694e00821d67cb150559
Chidera Christian <chiderachristain36@gmail.com>
2025-06-11 08:51:59 +0100

    chages made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function Header() {
   return (
     <>
-    
+     <div id="preloader-active">
+        <div className="preloader d-flex align-items-center justify-content-center">
+            <div className="preloader-inner position-relative">
+                <div className="preloader-circle"></div>
+                <div className="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.png" alt="logo"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
       <header>
         <div class="header-area header-transparent pt-20">
             <div class="main-header header-sticky">
